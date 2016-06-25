4d96a3d587ad784f9005cc46ef2e992a8d16be60
BenitoSilva <benito.silva.cuevas@hotmail.com>
2016-06-25 15:47:39 -0500

    Initial Commit

---
+++ Coursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coursera
+//
+//  Created by Benito Silva Cuevas on 25/06/16.
+//  Copyright © 2016 Benito Silva Cuevas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
